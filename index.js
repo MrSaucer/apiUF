e62afca0cb609960f5fd6101488073ef1dafa717
MrSaucer <108151584+MrSaucer@users.noreply.github.com>
2023-04-13 16:50:10 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 import express from 'express';
-import { buscarUfPorId, buscarUfsFiltrados } from './servicos/servico.js';
+import { buscarUfs, buscarUfPorId, buscarUfsPorSigla } from './servicos/servico.js';
 
 const app = express();
 
+app.get('/uf/', (req, res) => {
+  const sigla = req.query.sigla;
+  const resultado = sigla ? buscarUfsPorSigla(sigla) : buscarUfs();
+  
+  if (resultado) {
+    res.send(resultado);
+  } else {
+    res.status(404).send('UF não encontrada');
+  }
+});
 
-app.get('/uf', (req, res) => {
-    const filtros = req.query;
-    const ufsFiltrados = buscarUfsFiltrados(filtros);
-    if (ufsFiltrados) {
-      res.send(ufsFiltrados);
+  
+  app.get('/uf/:id', (req, res) => {
+    const id = req.params.id;
+    const resultado = buscarUfPorId(id);
+
+    if (resultado) {
+      res.send(resultado);
     } else {
       res.status(404).send('UF não encontrada');
     }
   });
   
-
-// Rota para buscar um estado pelo ID
-app.get('/uf/:id', (req, res) => {
-    const id = req.params.id;
-    const resultado = buscarUfPorId(id);
-    if (resultado) {
-        res.send(resultado);
-    } else {
-        res.status(404).send('UF não encontrada');
-    }
-});
-
-app.listen(8080, () => {
+  app.listen(8080, () => {
     console.log('Servidor iniciado na porta 8080');
 });
--- servicos/servico.js
+++ servicos/servico.js
@@ -1,35 +1,16 @@
 import { colecaoUf } from '../dados/dados.js';
 
+export const buscarUfs = () => {
+  return colecaoUf;
+}
 
-export const buscarUfsFiltrados = (filtros) => {
-  let ufs = colecaoUf;
-  if (filtros) {
-    // Verificar se o parâmetro "sigla" foi enviado nos filtros
-    if (filtros.sigla) {
-      // Verificar se a sigla informada é válida
-      const ufValida = colecaoUf.some((uf) => uf.sigla === filtros.sigla);
-      if (!ufValida) {
-        return null; // retorna null caso a sigla informada seja inválida
-      }
-      // Filtrar os estados com base na sigla
-      ufs = ufs.filter((uf) => uf.sigla === filtros.sigla);
-    }
-    // Verificar se o parâmetro "nome" foi enviado nos filtros
-    if (filtros.nome) {
-      // Filtrar os estados com base no nome
-      ufs = ufs.filter((uf) => uf.nome === filtros.nome);
-    }
-  }
-  return ufs;
+export const buscarUfsPorSigla = (sigla) => {
+  const ufsFiltradas = colecaoUf.filter((uf) => uf.sigla === sigla);
+  return ufsFiltradas.length > 0 ? ufsFiltradas : null; // retorna null caso não haja UF's com a sigla informada
 };
 
-// Controlador para buscar um estado pelo ID
 export const buscarUfPorId = (id) => {
-  // Converter o parâmetro "id" para o tipo "number"
   const ufId = parseInt(id);
-
-  // Buscar o estado correspondente ao ID nos dados
   const ufById = colecaoUf.find((uf) => uf.id === ufId);
-
   return ufById;
-};
+}
