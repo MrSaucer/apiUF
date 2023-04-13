d26c88a934cb9845b84233d2b9921aa52a9c5538
MrSaucer <108151584+MrSaucer@users.noreply.github.com>
2023-04-13 11:38:06 -0300

    Add files via upload

---
+++ data/dados.js
@@ -0,0 +1,244 @@
+export const uf = [
+    {
+        "id": 1,
+        "uf": "AC",
+        "nome": "Acre",
+        "regiao": "Norte",
+        "populacao": "928518",
+        "capital": "Rio Branco",
+        "principal_ponto_turistico": "Parque Nacional da Serra do Divisor"
+    },
+    {
+        "id": 2,
+        "uf": "AL",
+        "nome": "Alagoas",
+        "regiao": "Nordeste",
+        "populacao": "3388186",
+        "capital": "Maceió",
+        "principal_ponto_turistico": "Praia do Gunga"
+    },
+    {
+        "id": 3,
+        "uf": "AM",
+        "nome": "Amazonas",
+        "regiao": "Norte",
+        "populacao": "4378338",
+        "capital": "Manaus",
+        "principal_ponto_turistico": "Teatro Amazonas"
+    },
+    {
+        "id": 4,
+        "uf": "AP",
+        "nome": "Amapá",
+        "regiao": "Norte",
+        "populacao": "905294",
+        "capital": "Macapá",
+        "principal_ponto_turistico": "Fortaleza de São José de Macapá"
+    },
+    {
+        "id": 5,
+        "uf": "BA",
+        "nome": "Bahia",
+        "regiao": "Nordeste",
+        "populacao": "15072591",
+        "capital": "Salvador",
+        "principal_ponto_turistico": "Pelourinho"
+    },
+    {
+        "id": 6,
+        "uf": "CE",
+        "nome": "Ceará",
+        "regiao": "Nordeste",
+        "populacao": "9331146",
+        "capital": "Fortaleza",
+        "principal_ponto_turistico": "Beach Park"
+    },
+    {
+        "id": 7,
+        "uf": "DF",
+        "nome": "Distrito Federal",
+        "regiao": "Centro-Oeste",
+        "populacao": "3159008",
+        "capital": "Brasília",
+        "principal_ponto_turistico": "Congresso Nacional"
+    },
+    {
+        "id": 8,
+        "uf": "ES",
+        "nome": "Espírito Santo",
+        "regiao": "Sudeste",
+        "populacao": "4184440",
+        "capital": "Vitória",
+        "principal_ponto_turistico": "Convento da Penha"
+    },
+    {
+        "id": 9,
+        "uf": "GO",
+        "nome": "Goiás",
+        "regiao": "Centro-Oeste",
+        "populacao": "7368403",
+        "capital": "Goiânia",
+        "principal_ponto_turistico": "Parque Nacional da Chapada dos Veadeiros"
+    },
+    {
+        "id": 10,
+        "uf": "MA",
+        "nome": "Maranhão",
+        "regiao": "Nordeste",
+        "populacao": "7219066",
+        "capital": "São Luís",
+        "principal_ponto_turistico": "Lençóis Maranhenses"
+    },
+    {
+        "id": 11,
+        "uf": "MG",
+        "nome": "Minas Gerais",
+        "regiao": "Sudeste",
+        "populacao": "21609182",
+        "capital": "Belo Horizonte",
+        "principal_ponto_turistico": "Inhotim"
+    },
+    {
+        "id": 12,
+        "uf": "MS",
+        "nome": "Mato Grosso do Sul",
+        "regiao": "Centro-Oeste",
+        "populacao": "2890084",
+        "capital": "Campo Grande",
+        "principal_ponto_turistico": "Pantanal"
+    },
+    {
+        "id": 13,
+        "uf": "MT",
+        "nome": "Mato Grosso",
+        "regiao": "Centro-Oeste",
+        "populacao": "3637651",
+        "capital": "Cuiabá",
+        "principal_ponto_turistico": "Chapada dos Guimarães"
+    },
+    {
+        "id": 14,
+        "uf": "PA",
+        "nome": "Pará",
+        "regiao": "Norte",
+        "populacao": "8926319",
+        "capital": "Belém",
+        "principal_ponto_turistico": "Ilha de Marajó"
+    },
+    {
+        "id": 15,
+        "uf": "PB",
+        "nome": "Paraíba",
+        "regiao": "Nordeste",
+        "populacao": "4094681",
+        "capital": "João Pessoa",
+        "principal_ponto_turistico": "Praia de Tambaba"
+    },
+    {
+        "id": 16,
+        "uf": "PE",
+        "nome": "Pernambuco",
+        "regiao": "Nordeste",
+        "populacao": "9773805",
+        "capital": "Recife",
+        "principal_ponto_turistico": "Porto de Galinhas"
+    },
+    {
+        "id": 17,
+        "uf": "PI",
+        "nome": "Piauí",
+        "regiao": "Nordeste",
+        "populacao": "3300712",
+        "capital": "Teresina",
+        "principal_ponto_turistico": "Parque Nacional da Serra da Capivara"
+    },
+    {
+        "id": 18,
+        "uf": "PR",
+        "nome": "Paraná",
+        "regiao": "Sul",
+        "populacao": "11733800",
+        "capital": "Curitiba",
+        "principal_ponto_turistico": "Cataratas do Iguaçu"
+    },
+    {
+        "id": 19,
+        "uf": "RJ",
+        "nome": "Rio de Janeiro",
+        "regiao": "Sudeste",
+        "populacao": "17623041",
+        "capital": "Rio de Janeiro",
+        "principal_ponto_turistico": "Cristo Redentor"
+    },
+    {
+        "id": 20,
+        "uf": "RN",
+        "nome": "Rio Grande do Norte",
+        "regiao": "Nordeste",
+        "populacao": "3606574",
+        "capital": "Natal",
+        "principal_ponto_turistico": "Parque das Dunas"
+    },
+    {
+        "id": 21,
+        "uf": "RO",
+        "nome": "Rondônia",
+        "regiao": "Norte",
+        "populacao": "1847350",
+        "capital": "Porto Velho",
+        "principal_ponto_turistico": "Parque Natural de Porto Velho"
+    },
+    {
+        "id": 22,
+        "uf": "RR",
+        "nome": "Roraima",
+        "regiao": "Norte",
+        "populacao": "675576",
+        "capital": "Boa Vista",
+        "principal_ponto_turistico": "Monte Roraima"
+    },
+    {
+        "id": 23,
+        "uf": "RS",
+        "nome": "Rio Grande do Sul",
+        "regiao": "Sul",
+        "populacao": "11537887",
+        "capital": "Porto Alegre",
+        "principal_ponto_turistico": "Gramado"
+    },
+    {
+        "id": 24,
+        "uf": "SC",
+        "nome": "Santa Catarina",
+        "regiao": "Sul",
+        "populacao": "7485510",
+        "capital": "Florianópolis",
+        "principal_ponto_turistico": "Balneário Camboriú"
+    },
+    {
+        "id": 25,
+        "uf": "SE",
+        "nome": "Sergipe",
+        "regiao": "Nordeste",
+        "populacao": "2372088",
+        "capital": "Aracaju",
+        "principal_ponto_turistico": "Canion do Xingó"
+    },
+    {
+        "id": 26,
+        "uf": "SP",
+        "nome": "São Paulo",
+        "regiao": "Sudeste",
+        "populacao": "47256826",
+        "capital": "São Paulo",
+        "principal_ponto_turistico": "Avenida Paulista"
+    },
+    {
+        "id": 27,
+        "uf": "TO",
+        "nome": "Tocantins",
+        "regiao": "Norte",
+        "populacao": "1636784",
+        "capital": "Palmas",
+        "principal_ponto_turistico": "Cachoeira do Roncador"
+    }]
